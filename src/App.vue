7a1e4e13f8b17db95ec483c7581bd785daa177b8
AnhDuong36 <haiiyenn210@gmail.com>
2023-03-10 20:30:12 +0700

    feat:Da hoan thanh

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,49 @@
 <template>
-  <div>
-    <Header/>
-<SileBar />
-<router-view></router-view>
+ <Login v-if="!user" @show="getHome($event)" />
+  <div v-if="user">
+    <Header />
+    <SileBar />
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
 import Header from "./components/Header.vue";
 import SileBar from "./components/SileBar.vue";
+import Login from "./view/Login";
+import router from "./router";
 export default {
   name: 'App',
   components: {
     Header,
     SileBar,
-  }
+    Login,
+  },
+  data() {
+    return {
+      user: JSON.parse(localStorage.getItem("user")),
+      isShowHome: false,
+      check_user: null,
+    };
+  },
+  mounted() {
+    if (this.user) {
+      router.push("/");
+    } else {
+      router.push("/login");
+    }
+  },
+  methods: {
+    getHome(data) {
+      if (data) {
+        this.check_user = data;
+        this.isShowHome = true;
+        router.push("/");
+      } else {
+        this.isShowHome = false;
+      }
+    },
+  },
 }
 </script>
 
